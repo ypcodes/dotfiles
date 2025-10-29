685661242ad1c420ec1f488fadafcc8a4a2e7b95
Peng Ye <esszp@foxmail.com>
2025-10-29 14:00:03 +0800

    Automated backup of .config: 2025-10-29 14:00:03

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,8 +1,8 @@
 # 基础别名
 #alias find='fd'
 #alias grep='rg'
-#alias cat='bat'
-#alias tree='exa --tree'
+alias cat='bat'
+alias tree='exa --tree'
 alias e=$EDITOR
 alias et='emacsclient -t -a ""'
 alias vi=nvim
